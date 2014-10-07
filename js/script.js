78e9efed8b0ab7099e62ba7ab400ba9081619b2a
Rainer <rainer@websimple.be>
2014-10-07 18:34:20 +0200

    enlever l'alert

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
-    alert('ok');
+    //alert('ok');
   
   }
 };
