82baa1961abcb2da8bc45ff1fc9754b839ebc691
Rainer Müller <rainer@websimple.be>
2014-10-10 13:41:45 +0200

    transitions

--- js/script.js
+++ js/script.js
@@ -1,30 +1,59 @@
 /**
+
  * @file
+
  * A JavaScript file for the theme.
+
  *
+
  * In order for this JavaScript to be loaded on pages, see the instructions in
+
  * the README.txt next to this file.
+
  */
 
+
+
 // JavaScript should be made compatible with libraries other than jQuery by
+
 // wrapping it with an "anonymous closure". See:
+
 // - https://drupal.org/node/1446420
+
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+
 (function ($, Drupal, window, document, undefined) {
 
 
+
+
+
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
+
 Drupal.behaviors.my_custom_behavior = {
+
   attach: function(context, settings) {
+
     $('a.gallery').click(function(){
+
     	var id=$(this).attr('data-id');
-    	$('.hidden').hide();
-    	$('#image'+id).show();
+
+    	$('.hidden').hide(800);
+
+    	$('#image'+id).show(800);
+
     
+
     });
+
   
+
   }
+
 };
+
+
+
 
 
 })(jQuery, Drupal, this, this.document);
